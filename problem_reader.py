acd7a4655971797031e24ba42ef3e25321abc293
Dustin Scharf <d.scharf.2002@gmail.com>
2022-06-30 03:40:08 +0200

    Now processing csv as dataframe for speed

--- problem_reader.py
+++ problem_reader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from problem import Problem
 
@@ -11,14 +12,36 @@ class ProblemReader(object):
         self.directory: str = directory
 
     def read_problem(self, csv_file_name: str) -> Problem:
-        data = np.genfromtxt(csv_file_name, delimiter=',')
+        data = pd.read_csv(csv_file_name)
 
-        # can be 1 (for max) or -1 (for min) so c is directly converted to a max problem
-        max_min = np.array(data[0][-1])
-        c = np.multiply(np.array(data[0][:-1]), max_min)
+        pre_c = data[data['type'].str.match('z')]
+        if pre_c.shape[0] != 1:
+            print("No Z-function or more then 1 Z-function in CSV, exit...")
+            exit(1)
 
-        A = np.array(data[1:data.shape[0], 0:data.shape[1] - 1])
+        max_min = 1 if pre_c.iloc[0][-1] == 'max' else -1
+        c = np.multiply(pd.DataFrame.to_numpy(pre_c.drop(pre_c.columns[[-1, -2]], axis=1), dtype=np.float64),
+                        max_min).flatten()
 
-        b = np.array(data[1:, -1])
+        pre_u = data[data['b'].str.match('u')]
+        if pre_u.shape[0] != 1:
+            print("No upper bounds or more then 1 upper bounds per variable in CSV, exit...")
+            exit(1)
+        # TODO add to problem and add datatype np.float64, pay respect to unrestricted variables
+        u = pd.DataFrame.to_numpy(pre_u.drop(pre_u.columns[[-1, -2]], axis=1)).flatten()
 
-        return Problem(c, A, b)
+        pre_l = data[data['b'].str.match('l')]
+        if pre_l.shape[0] != 1:
+            print("No lower bounds or more then 1 lower bounds per variable in CSV, exit...")
+            exit(1)
+        # TODO add to problem and add datatype np.float64, pay respect to unrestricted variables
+        l = pd.DataFrame.to_numpy(pre_l.drop(pre_l.columns[[-1, -2]], axis=1)).flatten()
+
+        A_le_pre = data[data['type'].str.match('<=')]
+        A_le = pd.DataFrame.to_numpy(A_le_pre.drop(A_le_pre.columns[[-1, -2]], axis=1), dtype=np.float64)
+
+        b_le_pre = data[data['type'].str.match('<=')]
+        b_le = pd.Series.to_numpy(b_le_pre['b'], dtype=np.float64).flatten()
+
+        # TODO add bounds
+        return Problem(c, A_le, b_le)
