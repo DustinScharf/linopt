1c3afe49ce675dd93b49ef864e6d07653ca103f9
Dustin Scharf <d.scharf.2002@gmail.com>
2022-07-07 00:15:52 +0200

    Added new problem

--- quicktest.py
+++ quicktest.py
@@ -10,3 +10,5 @@ if __name__ == '__main__':
     print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue3_a1.csv")).z, 4))
     print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue2_a2.csv")).z, 211))
     print(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ha6_a1.csv")).z == "UNBOUNDED")
+    print()
+    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ha6_a2.csv")).z, 30))
