e980e528ad12c70fb3d594614a8b9b01532fa224
Dustin Scharf <d.scharf.2002@gmail.com>
2022-07-06 23:59:21 +0200

    Added small test to run on thy fly from time to time

---
+++ quicktest.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+import problem_reader
+from problem_solver import ProblemSolver
+
+if __name__ == '__main__':
+    problem_reader = problem_reader.ProblemReader()
+    problem_solver = ProblemSolver()
+
+    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue3_a1.csv")).z, 4))
+    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue2_a2.csv")).z, 211))
+    print(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ha6_a1.csv")).z == "UNBOUNDED")
