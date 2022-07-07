88b01e06a984083d43090e44533e99edebd19f24
Dustin Scharf <d.scharf.2002@gmail.com>
2022-07-07 04:36:11 +0200

    Added new problem and test

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from problem_solver import ProblemSolver, Methods
 if __name__ == '__main__':
     problem_reader = ProblemReader()
 
-    # problem = problem_reader.read_problem("btu_ss2022_opt1_ue5_a1.csv")  # 2440
-    # problem = problem_reader.read_problem("btu_ss2022_opt1_ue4_a2.csv")  # 58
+    problem = problem_reader.read_problem("btu_ss2022_opt1_ue1_a1.csv")  # 32
     # problem = problem_reader.read_problem("btu_ss2022_opt1_ue2_a2.csv")  # 211
+    # problem = problem_reader.read_problem("btu_ss2022_opt1_ue3_a1.csv")  # 4
+    # problem = problem_reader.read_problem("btu_ss2022_opt1_ue4_a2.csv")  # 58
+    # problem = problem_reader.read_problem("btu_ss2022_opt1_ue5_a1.csv")  # 2440
     # problem = problem_reader.read_problem("btu_ss2022_opt1_ha6_a1.csv")  # UNBOUNDED
-    problem = problem_reader.read_problem("btu_ss2022_opt1_ue3_a1.csv")  # 4
     # problem = problem_reader.read_problem("btu_ss2022_opt1_ha6_a2.csv")  # 30
 
     problem_solver = ProblemSolver()
--- quicktest.py
+++ quicktest.py
@@ -9,11 +9,20 @@ if __name__ == '__main__':
 
     print("===== TEST START =====")
 
+    # UE
+    # 1
+    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue1_a1.csv")).z, 32))
+    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue2_a2.csv")).z, 211))
+    # 3
     print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue3_a1.csv")).z, 4))
-    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue2_a2.csv")).z, 211))
-    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ha6_a2.csv")).z, 30))
-    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue2_a2.csv")).z, 211))
+    # 4
     print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue4_a2.csv")).z, 58))
+    # 5
+    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ue5_a1.csv")).z, 2440))
+
+    # HA
+    # 6
     print(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ha6_a1.csv")).z == "UNBOUNDED")
+    print(np.isclose(problem_solver.solve(problem_reader.read_problem("btu_ss2022_opt1_ha6_a2.csv")).z, 30))
 
     print("===== TEST END =====")
